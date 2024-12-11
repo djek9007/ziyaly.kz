4cbd1026446bcdf0ceec54be31e526b1f6172975
djek9007 <djek9007@gmail.com>
2024-12-11 23:55:26 +0500

    ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split(',')

--- ziyaly/wsgi.py
+++ ziyaly/wsgi.py
@@ -11,7 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ziyaly.kz.ziyaly.settings')
-
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ziyaly.settings')
 
 application = get_wsgi_application()
